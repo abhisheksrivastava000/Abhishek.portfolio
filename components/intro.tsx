0cd6538ba6e7c626ae0683e877ca8b264a65d613
abhishek-0- <abhishek.srivastava2@s.amity.edu>
2025-06-18 09:19:29 +0530

    changed github address

--- components/intro.tsx
+++ components/intro.tsx
@@ -97,7 +97,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/000-ghost-000"
+          href="https://github.com/abhisheksrivastava000"
           target="_blank"
         >
           <FaGithubSquare />
